20ae5a25c66e847a04b8625aabd03df4e33634e2
5almiakki <jpui9635@gmail.com>
2026-02-12 02:11:02 +0900

    feat: 업종 선택 버튼 추가 #2

--- frontend/multi-child-family-welfare-map/src/components/views/home/SearchInput.vue
+++ frontend/multi-child-family-welfare-map/src/components/views/home/SearchInput.vue
@@ -1,5 +1,10 @@
-<script>
+<script setup>
+import { ref } from 'vue';
 
+const categories = ref([
+  "금융기관", "문화시설", "체육시설", "유치원", "어린이집", "학원", "요식업등", "병의원", "한의원",
+  "이미용업", "세탁, 목욕업", "자동차", "사진관", "유통업체", "안경", "여행업", "숙박업", "기타",
+]);
 </script>
 
 <template>
@@ -7,5 +12,33 @@
     <BFormInput
       placeholder="검색어를 입력하세요."
     />
+    <div class="d-flex flex-nowrap overflow-auto pb-2 custom-scrollbar button-group">
+      <BButton
+        v-for="(item, index) in categories"
+        :key="index"
+        variant="light"
+        pill
+        size="sm"
+        class="m-1 shadow-sm text-nowrap border"
+      >
+        {{ item }}
+      </BButton>
+    </div>
   </BForm>
 </template>
+
+<style scoped>
+.custom-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+.custom-scrollbar {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+.button-group {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+  margin-top: 10px;
+}
+</style>
