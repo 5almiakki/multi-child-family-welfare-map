002d837db50c8876f0e5e67c28ae75b05ac5761c
5almiakki <jpui9635@gmail.com>
2026-02-13 02:25:55 +0900

    feat: 검색 버튼 추가 #2

--- frontend/multi-child-family-welfare-map/src/components/views/home/SearchInput.vue
+++ frontend/multi-child-family-welfare-map/src/components/views/home/SearchInput.vue
@@ -1,17 +1,28 @@
 <script setup>
 import { ref } from 'vue';
 
+const searchText = ref("");
 const categories = ref([
   "금융기관", "문화시설", "체육시설", "유치원", "어린이집", "학원", "요식업등", "병의원", "한의원",
   "이미용업", "세탁, 목욕업", "자동차", "사진관", "유통업체", "안경", "여행업", "숙박업", "기타",
 ]);
+
+const search = () => {
+
+};
 </script>
 
 <template>
   <BForm>
-    <BFormInput
-      placeholder="검색어를 입력하세요."
-    />
+    <BInputGroup>
+      <BFormInput
+        v-model="searchText"
+        placeholder="검색어를 입력하세요."
+      />
+      <template #append>
+        <BButton variant="primary" @click="search">검색</BButton>
+      </template>
+    </BInputGroup>
     <div class="d-flex flex-nowrap overflow-auto pb-2 custom-scrollbar button-group">
       <BButton
         v-for="(item, index) in categories"
