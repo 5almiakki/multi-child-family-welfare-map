800fd1945bd9bc84a654551979b38a1fbcd92baa
5almiakki <jpui9635@gmail.com>
2026-02-13 21:49:44 +0900

    feat: 검색 시 검색 결과 컴포넌트 보이기 #4

--- frontend/multi-child-family-welfare-map/src/components/views/home/SearchResult.vue
+++ frontend/multi-child-family-welfare-map/src/components/views/home/SearchResult.vue
@@ -1,11 +1,29 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  modelValue: Boolean,
+  searchData: Array
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const isOpen = computed({
+  get: () => props.modelValue,
+  set: (val) => emit('update:modelValue', val)
+});
+</script>
+
 <template>
   <BOffcanvas
     id="search-result"
+    class="shadow-lg search-result"
     title="검색 결과"
     placement="bottom"
-    show
+    v-model="isOpen"
     no-backdrop
-    shadow
   >
+    <div class="result-list">
+    </div>
   </BOffcanvas>
 </template>
