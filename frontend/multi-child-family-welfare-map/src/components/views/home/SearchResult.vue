dd4b73b1f12767b95501aa1aa4491a8b9419a797
5almiakki <jpui9635@gmail.com>
2026-02-21 21:53:35 +0900

    feat: 검색 항목과 구분선을 검색 결과 컴포넌트에 추가 #4

--- frontend/multi-child-family-welfare-map/src/components/views/home/SearchResult.vue
+++ frontend/multi-child-family-welfare-map/src/components/views/home/SearchResult.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { useSearchResultStore } from '@/stores/searchResult';
 import { computed } from 'vue';
+import SearchResultItem from './SearchResultItem.vue';
 
 const props = defineProps({
   modelValue: Boolean
 });
 
 const emit = defineEmits(['update:modelValue']);
+
+const searchResultStore = useSearchResultStore();
 
 const isOpen = computed({
   get: () => props.modelValue,
@@ -22,7 +26,12 @@ const isOpen = computed({
     v-model="isOpen"
     no-backdrop
   >
-    <div class="result-list">
+    <div v-for="(searchResult, index) in searchResultStore.searchResults">
+      <SearchResultItem
+        :key="index"
+        :search-result-item="searchResult"
+      />
+      <hr v-if="index < searchResultStore.searchResults.length - 1">
     </div>
   </BOffcanvas>
 </template>
