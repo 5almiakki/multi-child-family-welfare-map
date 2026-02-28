005bd1415fa590201f1d8ce4c0e187b3201983ef
5almiakki <jpui9635@gmail.com>
2026-02-28 17:59:02 +0900

    init(be): 프로젝트 생성 #7

---
+++ backend/dagagomap/src/test/java/kr/dagagomap/DagagomapApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.dagagomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagagomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
