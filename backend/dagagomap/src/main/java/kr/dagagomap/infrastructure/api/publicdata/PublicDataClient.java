a18a87ddb40ca9cd4a9da433207430d8e064e191
5almiakki <jpui9635@gmail.com>
2026-03-07 22:19:46 +0900

    feat(be): 컴포넌트 내에서 RestClient 직접 구성 #7

--- backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/publicdata/PublicDataClient.java
+++ backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/publicdata/PublicDataClient.java
@@ -1,10 +1,11 @@
-package kr.dagagomap.infrastructure.api;
+package kr.dagagomap.infrastructure.api.publicdata;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.xml.JacksonXmlHttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
-import kr.dagagomap.infrastructure.api.dto.ApiResponse;
+import kr.dagagomap.infrastructure.api.publicdata.dto.PublicDataResponse;
 
 @Component
 public class PublicDataClient {
@@ -15,16 +16,20 @@ public class PublicDataClient {
 	public PublicDataClient(
 			@Value("${custom.public-data-service-key}")
 			String publicDataServicekey,
-			RestClient restClient) {
+			RestClient.Builder restClientBuilder) {
 		this.publicDataServicekey = publicDataServicekey;
-		this.restClient = restClient;
+		this.restClient = restClientBuilder
+				.configureMessageConverters(configurer -> configurer
+						.withXmlConverter(new JacksonXmlHttpMessageConverter()))
+				.baseUrl("http://apis.data.go.kr")
+				.build();
 	}
 
-	public ApiResponse getFamilyLoveCardInfo() {
+	public PublicDataResponse getFamilyLoveCardInfo() {
 		String uri = "/6260000/BusanFmlyLvcrInfoService/getFmlyLvcrInfo?ServiceKey="
 				+ publicDataServicekey
 				+ "&pageNo=1&numOfRows=9999";
-		ApiResponse response = restClient.get().uri(uri).retrieve().body(ApiResponse.class);
+		PublicDataResponse response = restClient.get().uri(uri).retrieve().body(PublicDataResponse.class);
 		return response;
 	}
 
