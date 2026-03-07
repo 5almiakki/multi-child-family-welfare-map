f0e8aea6df1e962fcca9795a8a681c6ed8faaebc
5almiakki <jpui9635@gmail.com>
2026-03-07 23:23:51 +0900

    doc(be): 공공데이터 응답 record Javadoc 작성 #7

--- backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/publicdata/busan/dto/BusanPublicDataResponse.java
+++ backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/publicdata/busan/dto/BusanPublicDataResponse.java
@@ -2,41 +2,81 @@ package kr.dagagomap.infrastructure.api.publicdata.busan.dto;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
 
+/**
+ * 참여업체명, 우대 내용 등 부산광역시 가족사랑카드참여업체 현황 조회 정보
+ * 
+ * @param header 응답 관련 정보
+ * @param body 응답 결과
+ */
 @JsonRootName("response")
 public record BusanPublicDataResponse(
 		Header header,
 		Body body
 ) {
 
+	/**
+	 * 응답 관련 정보
+	 * 
+	 * @param resultCode 결과코드
+	 * @param resultMsg 결과메시지
+	 */
 	public static record Header(
-			Integer resultCode, // 결과코드
-			String resultMsg // 결과메시지
+			Integer resultCode,
+			String resultMsg
 	) {}
 
+	/**
+	 * 응답 결과
+	 * 
+	 * @param totalCount 전체 결과 수
+	 * @param numOfRows 한 페이지 결과 수
+	 * @param pageNo 페이지 번호
+	 * @param items 업체 정보들
+	 */
 	public static record Body(
-			Integer totalCount, // 전체 결과 수
-			Integer numOfRows, // 한 페이지 결과 수
-			Integer pageNo, // 페이지 번호
+			Integer totalCount,
+			Integer numOfRows,
+			Integer pageNo,
 			Item[] items
 	) {
 
+		/**
+		 * 업체 정보
+		 * 
+		 * @param cpCompname 참여업체명
+		 * @param cpHome URL
+		 * @param cpClass 업종코드
+		 * @param cpHgu 지역
+		 * @param cpCeoname 대표자명
+		 * @param cpSanum 사업자번호
+		 * @param cpSidate 시행일
+		 * @param cpAddr 주소
+		 * @param cpTel 연락처
+		 * @param cpEmail 이메일
+		 * @param cpEmailflag 이메일 수신여부
+		 * @param cpInfo 회사소개
+		 * @param cpWoo 우대 내용
+		 * @param cpState 사용여부
+		 * @param cpImg 첨부파일
+		 * @param cpWebflag 승인여부
+		 */
 		public static record Item(
-				String cpCompname, // 참여업체명
-				String cpHome, // URL
-				String cpClass, // 업종코드
-				String cpHgu, // 지역
-				String cpCeoname, // 대표자명
-				String cpSanum, // 사업자번호
-				String cpSidate, // 시행일
-				String cpAddr, // 주소
-				String cpTel, // 연락처
-				String cpEmail, // 이메일
-				String cpEmailflag, // 이메일 수신여부
-				String cpInfo, // 회사소개
-				String cpWoo, // 우대 내용
-				String cpState, // 사용여부
-				String cpImg, // 첨부파일
-				String cpWebflag // 승인여부
+				String cpCompname,
+				String cpHome,
+				String cpClass,
+				String cpHgu,
+				String cpCeoname,
+				String cpSanum,
+				String cpSidate,
+				String cpAddr,
+				String cpTel,
+				String cpEmail,
+				String cpEmailflag,
+				String cpInfo,
+				String cpWoo,
+				String cpState,
+				String cpImg,
+				String cpWebflag
 		) {}
 
 	}
