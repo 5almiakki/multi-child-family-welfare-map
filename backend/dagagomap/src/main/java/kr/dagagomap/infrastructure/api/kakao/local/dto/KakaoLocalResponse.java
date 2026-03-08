b793e619db0d91bccee968bd7e3413eb4bb2402f
5almiakki <jpui9635@gmail.com>
2026-03-08 15:52:27 +0900

    feat(be): Kakao Local API 응답 매핑 클래스 애너테이션 추가 #7

--- backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/kakao/local/dto/KakaoLocalResponse.java
+++ backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/kakao/local/dto/KakaoLocalResponse.java
@@ -1,14 +1,20 @@
 package kr.dagagomap.infrastructure.api.kakao.local.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import tools.jackson.databind.PropertyNamingStrategies;
+import tools.jackson.databind.annotation.JsonNaming;
+
 /**
  * 주소를 좌표로 변환하는 Kakao Local API 응답
  * 
  * @param meta 응답 관련 정보
  * @param document 응답 결과
  */
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoLocalResponse(
 		Meta meta,
-		Document[] document
+		Document[] documents
 ) {
 	
 	/**
@@ -18,6 +24,7 @@ public record KakaoLocalResponse(
 	 * @param pageableCount total_count 중 노출 가능 문서 수
 	 * @param isEnd 현재 페이지가 마지막 페이지인지 여부
 	 */
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static record Meta(
 			Integer totalCount,
 			Integer pageableCount,
@@ -36,6 +43,7 @@ public record KakaoLocalResponse(
 	 * @param address 지번 주소 상세 정보
 	 * @param roadAddress 도로명 주소 상세 정보
 	 */
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 	public static record Document(
 			String addressName,
 			String addressType,
@@ -61,12 +69,17 @@ public record KakaoLocalResponse(
 		 * @param x X 좌표값, 경위도인 경우 경도(longitude)
 		 * @param y Y 좌표값, 경위도인 경우 위도(latitude)
 		 */
+		@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 		public static record Address(
 				String addressName,
-				String region1depthName,
-				String region2depthName,
-				String region3depthName,
-				String region3depthHName,
+				@JsonProperty("region_1depth_name")
+				String region1DepthName,
+				@JsonProperty("region_2depth_name")
+				String region2DepthName,
+				@JsonProperty("region_3depth_name")
+				String region3DepthName,
+				@JsonProperty("region_3depth_h_name")
+				String region3DepthHName,
 				String hCode,
 				String bCode,
 				String mountainYn,
@@ -92,11 +105,15 @@ public record KakaoLocalResponse(
 		 * @param x X 좌표값, 경위도인 경우 경도(longitude)
 		 * @param y Y 좌표값, 경위도인 경우 위도(latitude)
 		 */
+		@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 		public static record RoadAddress(
 				String addressName,
-				String region1depthName,
-				String region2depthName,
-				String region3depthName,
+				@JsonProperty("region_1depth_name")
+				String region1DepthName,
+				@JsonProperty("region_2depth_name")
+				String region2DepthName,
+				@JsonProperty("region_3depth_name")
+				String region3DepthName,
 				String roadName,
 				String undergroundYn,
 				String mainBuildingNo,
