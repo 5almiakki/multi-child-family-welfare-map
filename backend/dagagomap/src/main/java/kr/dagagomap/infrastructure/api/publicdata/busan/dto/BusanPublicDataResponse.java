41c0631cffbd417ee0ceb721917d1eb8d7fe68d0
5almiakki <jpui9635@gmail.com>
2026-06-09 18:39:12 +0900

    docs(be): 공공데이터 응답 record 파라미터별 Javadoc 추가 #7

--- backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/publicdata/busan/dto/BusanPublicDataResponse.java
+++ backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/publicdata/busan/dto/BusanPublicDataResponse.java
@@ -4,45 +4,53 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 
 /**
  * 참여업체명, 우대 내용 등 부산광역시 가족사랑카드참여업체 현황 조회 정보
- * 
+ *
  * @param header 응답 관련 정보
  * @param body 응답 결과
  */
 @JsonRootName("response")
 public record BusanPublicDataResponse(
+		/** 응답 관련 정보 */
 		Header header,
+		/** 응답 결과 */
 		Body body
 ) {
 
 	/**
 	 * 응답 관련 정보
-	 * 
+	 *
 	 * @param resultCode 결과코드
 	 * @param resultMsg 결과메시지
 	 */
 	public static record Header(
+			/** 결과코드 */
 			Integer resultCode,
+			/** 결과메시지 */
 			String resultMsg
 	) {}
 
 	/**
 	 * 응답 결과
-	 * 
+	 *
 	 * @param totalCount 전체 결과 수
 	 * @param numOfRows 한 페이지 결과 수
 	 * @param pageNo 페이지 번호
 	 * @param items 업체 정보들
 	 */
 	public static record Body(
+			/** 전체 결과 수 */
 			Integer totalCount,
+			/** 한 페이지 결과 수 */
 			Integer numOfRows,
+			/** 페이지 번호 */
 			Integer pageNo,
+			/** 업체 정보들 */
 			Item[] items
 	) {
 
 		/**
 		 * 업체 정보
-		 * 
+		 *
 		 * @param cpCompname 참여업체명
 		 * @param cpHome URL
 		 * @param cpClass 업종코드
@@ -61,21 +69,37 @@ public record BusanPublicDataResponse(
 		 * @param cpWebflag 승인여부
 		 */
 		public static record Item(
+				/** 참여업체명 */
 				String cpCompname,
+				/** URL */
 				String cpHome,
+				/** 업종코드 */
 				String cpClass,
+				/** 지역 */
 				String cpHgu,
+				/** 대표자명 */
 				String cpCeoname,
+				/** 사업자번호 */
 				String cpSanum,
+				/** 시행일 */
 				String cpSidate,
+				/** 주소 */
 				String cpAddr,
+				/** 연락처 */
 				String cpTel,
+				/** 이메일 */
 				String cpEmail,
+				/** 이메일 수신여부 */
 				String cpEmailflag,
+				/** 회사소개 */
 				String cpInfo,
+				/** 우대 내용 */
 				String cpWoo,
+				/** 사용여부 */
 				String cpState,
+				/** 첨부파일 */
 				String cpImg,
+				/** 승인여부 */
 				String cpWebflag
 		) {}
 
