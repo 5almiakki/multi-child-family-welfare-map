1aeb5a10e7e7dce84aa895eacf46b632fdccda91
5almiakki <jpui9635@gmail.com>
2026-06-14 17:37:32 +0900

    test(be): 카카오 로컬 API 쿼터 및 rate limit 확인용 테스트 #7

---
+++ backend/dagagomap/src/test/java/kr/dagagomap/benchmark/KakaoLocalClientBenchmark.java
@@ -0,0 +1,148 @@
+package kr.dagagomap.benchmark;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.test.context.ActiveProfiles;
+
+import kr.dagagomap.infrastructure.api.kakao.local.KakaoLocalClient;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest(properties = {
+		"logging.level.kr.dagagomap.infrastructure.api.kakao.local=DEBUG"
+})
+@ActiveProfiles({ "secret" })
+@Tag("manual-benchmark")
+public class KakaoLocalClientBenchmark {
+
+	@Autowired
+	private KakaoLocalClient kakaoLocalClient;
+
+	@Autowired
+	@Qualifier("applicationTaskExecutor")
+	private AsyncTaskExecutor asyncTaskExecutor;
+
+	@Test
+	@DisplayName("주소를 좌표로 변환하는 API 쿼터 확인용")
+	void convertAddressToCoordinatesBenchmark() {
+		List<String> addresses = List.of(
+				"부산광역시 중구 중앙대로 1",
+				"부산광역시 중구 중앙대로 2",
+				"부산광역시 중구 중앙대로 3",
+				"부산광역시 중구 중앙대로 4",
+				"부산광역시 중구 중앙대로 5",
+				"부산광역시 서구 구덕로 6",
+				"부산광역시 서구 구덕로 7",
+				"부산광역시 서구 구덕로 8",
+				"부산광역시 서구 구덕로 9",
+				"부산광역시 서구 구덕로 10",
+				"부산광역시 동구 초량로 11",
+				"부산광역시 동구 초량로 12",
+				"부산광역시 동구 초량로 13",
+				"부산광역시 동구 초량로 14",
+				"부산광역시 동구 초량로 15",
+				"부산광역시 영도구 태종로 16",
+				"부산광역시 영도구 태종로 17",
+				"부산광역시 영도구 태종로 18",
+				"부산광역시 영도구 태종로 19",
+				"부산광역시 영도구 태종로 20",
+				"부산광역시 부산진구 중앙대로 21",
+				"부산광역시 부산진구 중앙대로 22",
+				"부산광역시 부산진구 중앙대로 23",
+				"부산광역시 부산진구 중앙대로 24",
+				"부산광역시 부산진구 중앙대로 25",
+				"부산광역시 동래구 명륜로 26",
+				"부산광역시 동래구 명륜로 27",
+				"부산광역시 동래구 명륜로 28",
+				"부산광역시 동래구 명륜로 29",
+				"부산광역시 동래구 명륜로 30",
+				"부산광역시 남구 수영로 31",
+				"부산광역시 남구 수영로 32",
+				"부산광역시 남구 수영로 33",
+				"부산광역시 남구 수영로 34",
+				"부산광역시 남구 수영로 35",
+				"부산광역시 북구 금곡대로 36",
+				"부산광역시 북구 금곡대로 37",
+				"부산광역시 북구 금곡대로 38",
+				"부산광역시 북구 금곡대로 39",
+				"부산광역시 북구 금곡대로 40",
+				"부산광역시 해운대구 해운대로 41",
+				"부산광역시 해운대구 해운대로 42",
+				"부산광역시 해운대구 해운대로 43",
+				"부산광역시 해운대구 해운대로 44",
+				"부산광역시 해운대구 해운대로 45",
+				"부산광역시 사하구 낙동대로 46",
+				"부산광역시 사하구 낙동대로 47",
+				"부산광역시 사하구 낙동대로 48",
+				"부산광역시 사하구 낙동대로 49",
+				"부산광역시 사하구 낙동대로 50",
+				"부산광역시 금정구 중앙대로 51",
+				"부산광역시 금정구 중앙대로 52",
+				"부산광역시 금정구 중앙대로 53",
+				"부산광역시 금정구 중앙대로 54",
+				"부산광역시 금정구 중앙대로 55",
+				"부산광역시 강서구 공항로 56",
+				"부산광역시 강서구 공항로 57",
+				"부산광역시 강서구 공항로 58",
+				"부산광역시 강서구 공항로 59",
+				"부산광역시 강서구 공항로 60",
+				"부산광역시 연제구 중앙대로 61",
+				"부산광역시 연제구 중앙대로 62",
+				"부산광역시 연제구 중앙대로 63",
+				"부산광역시 연제구 중앙대로 64",
+				"부산광역시 연제구 중앙대로 65",
+				"부산광역시 수영구 광안해변로 66",
+				"부산광역시 수영구 광안해변로 67",
+				"부산광역시 수영구 광안해변로 68",
+				"부산광역시 수영구 광안해변로 69",
+				"부산광역시 수영구 광안해변로 70",
+				"부산광역시 사상구 광장로 71",
+				"부산광역시 사상구 광장로 72",
+				"부산광역시 사상구 광장로 73",
+				"부산광역시 사상구 광장로 74",
+				"부산광역시 사상구 광장로 75",
+				"부산광역시 기장군 기장대로 76",
+				"부산광역시 기장군 기장대로 77",
+				"부산광역시 기장군 기장대로 78",
+				"부산광역시 기장군 기장대로 79",
+				"부산광역시 기장군 기장대로 80",
+				"부산광역시 중구 대청로 81",
+				"부산광역시 중구 대청로 82",
+				"부산광역시 동구 범일로 83",
+				"부산광역시 동구 범일로 84",
+				"부산광역시 부산진구 전포대로 85",
+				"부산광역시 부산진구 전포대로 86",
+				"부산광역시 남구 용소로 87",
+				"부산광역시 남구 용소로 88",
+				"부산광역시 해운대구 센텀중앙로 89",
+				"부산광역시 해운대구 센텀중앙로 90",
+				"부산광역시 수영구 수영로 91",
+				"부산광역시 수영구 수영로 92",
+				"부산광역시 연제구 월드컵대로 93",
+				"부산광역시 연제구 월드컵대로 94",
+				"부산광역시 금정구 금정로 95",
+				"부산광역시 금정구 금정로 96",
+				"부산광역시 사하구 다대로 97",
+				"부산광역시 사하구 다대로 98",
+				"부산광역시 강서구 명지국제로 99",
+				"부산광역시 기장군 정관로 100");
+		List<CompletableFuture<Void>> futures = new ArrayList<>();
+		for (int i = 0; i < 49; i++) {
+			int idx = i;
+			futures.add(CompletableFuture.runAsync(() -> {
+				kakaoLocalClient.convertAddressToCoordinates(addresses.get(idx));
+			}, asyncTaskExecutor));
+		}
+		CompletableFuture.allOf(futures.toArray(new CompletableFuture<?>[0])).join();
+	}
+
+}
