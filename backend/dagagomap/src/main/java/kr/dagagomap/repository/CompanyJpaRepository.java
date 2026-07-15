b4adb71591da481d4b4d9d348f48ae442e4662cd
5almiakki <jpui9635@gmail.com>
2026-07-15 17:23:08 +0900

    feat(be): 공공데이터에서 없어진 업체 반영을 위한 메서드 추가 #7

--- backend/dagagomap/src/main/java/kr/dagagomap/repository/CompanyJpaRepository.java
+++ backend/dagagomap/src/main/java/kr/dagagomap/repository/CompanyJpaRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.dagagomap.entity.Company;
 
+import java.util.List;
+
 public interface CompanyJpaRepository extends JpaRepository<Company, Long> {
+
+	List<Company> findAllByTaxIdNotIn(List<Long> taxIds);
 
 }
