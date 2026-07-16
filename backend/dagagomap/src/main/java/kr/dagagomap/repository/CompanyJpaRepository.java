17eb5de47d841734031ae4c4ac0232280dd09147
5almiakki <jpui9635@gmail.com>
2026-07-16 16:03:52 +0900

    feat(be): 더 이상 없는 업체 조회 시 컬렉션을 받게 파라미터 수정 #7

--- backend/dagagomap/src/main/java/kr/dagagomap/repository/CompanyJpaRepository.java
+++ backend/dagagomap/src/main/java/kr/dagagomap/repository/CompanyJpaRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.dagagomap.entity.Company;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface CompanyJpaRepository extends JpaRepository<Company, Long> {
 
-	List<Company> findAllByTaxIdNotIn(List<Long> taxIds);
+	List<Company> findAllByTaxIdNotIn(Collection<Long> taxIds);
 
 }
