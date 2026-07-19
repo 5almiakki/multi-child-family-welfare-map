17e9f4acc9b709c071e24ac4affaf7e58a03ab01
5almiakki <jpui9635@gmail.com>
2026-07-19 20:57:13 +0900

    config(be): CORS 설정 #10

---
+++ backend/dagagomap/src/main/java/kr/dagagomap/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package kr.dagagomap.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/v1/companies/**")
+				.allowedOrigins("http://localhost:5173")
+				.allowedMethods("GET");
+	}
+}
