78d7f300b8320b38d6168d222884d1d9e37e7183
5almiakki <jpui9635@gmail.com>
2026-07-22 15:34:04 +0900

    feat(fe): Axios로 companyService 생성 #16

---
+++ frontend/multi-child-family-welfare-map/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const { VITE_API_BASE_URL } = import.meta.env;
+
+const api = axios.create({
+  baseURL: VITE_API_BASE_URL,
+  timeout: 5000
+});
+
+export default api;
