2c0383c4406d85d281450c70b334dc3feb8a7143
5almiakki <jpui9635@gmail.com>
2026-07-30 03:48:40 +0900

    fix(be): gradlew build 실패 해결

--- backend/dagagomap/src/test/java/kr/dagagomap/DagagomapApplicationTests.java
+++ backend/dagagomap/src/test/java/kr/dagagomap/DagagomapApplicationTests.java
@@ -2,8 +2,10 @@ package kr.dagagomap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({ "test", "secret" })
 class DagagomapApplicationTests {
 
 	@Test
