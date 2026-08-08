09eadf8fbdc75cce9baadcf04c7bcbdaa527679d
5almiakki <jpui9635@gmail.com>
2026-08-08 22:28:21 +0900

    fix(be): 사업자번호 NPE 수정 #26

--- backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/publicdata/busan/dto/BusanPublicDataResponse.java
+++ backend/dagagomap/src/main/java/kr/dagagomap/infrastructure/api/publicdata/busan/dto/BusanPublicDataResponse.java
@@ -106,7 +106,7 @@ public record BusanPublicDataResponse(
 
 			public Company toCompany() {
 				return Company.builder()
-						.taxId(Long.valueOf(cpSanum))
+						.taxId(cpSanum == null ? null : Long.valueOf(cpSanum))
 						.name(cpCompname)
 						.homepageUrl(cpHome)
 						.category(cpClass)
