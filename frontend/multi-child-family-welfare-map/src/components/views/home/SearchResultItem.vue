ca23320728fa75cb38540b1f8c3e46011a662df9
5almiakki <jpui9635@gmail.com>
2026-08-25 22:37:07 +0900

    feat(fe): 검색 결과 항목 스타일 수정 #62

--- frontend/multi-child-family-welfare-map/src/components/views/home/SearchResultItem.vue
+++ frontend/multi-child-family-welfare-map/src/components/views/home/SearchResultItem.vue
@@ -1,14 +1,54 @@
 <script setup>
-const props = defineProps({
-  searchResultItem: Object
+defineProps({
+  searchResultItem: {
+    type: Object,
+    required: true
+  }
 });
 </script>
 
 <template>
-  <h5>{{ searchResultItem.name }}</h5>
-  <p v-if="searchResultItem.tel || searchResultItem.sourceAddress">
-    <span v-if="searchResultItem.tel">{{ searchResultItem.tel }}<br></span>
-    {{ searchResultItem.sourceAddress }}
-  </p>
-  <p>{{ searchResultItem.benefit }}</p>
+  <div class="result-item">
+    <div class="item-name mb-1">{{ searchResultItem.name }}</div>
+    <div class="item-info text-muted mb-1">
+      <span v-if="searchResultItem.tel" class="me-2">{{ searchResultItem.tel }}</span>
+      <span v-if="searchResultItem.sourceAddress">{{ searchResultItem.sourceAddress }}</span>
+    </div>
+    <div class="item-benefit text-success">
+      {{ searchResultItem.benefit }}
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.result-item {
+  height: 95px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  overflow: hidden;
+}
+
+.item-name {
+  font-weight: 600;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.item-info {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.item-benefit {
+  font-size: 0.85rem;
+  line-height: 1.35;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
